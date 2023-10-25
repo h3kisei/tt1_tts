5f45d8afb5c12d3ea37800740a4b2e0a1362efc4
HHoang <hoanghust218@gmail.com>
2023-10-25 11:59:33 +0700

    dk

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//next + prev button slider
 document.getElementById("next").onclick = function () {
   let lists = document.querySelectorAll(".item");
   document.getElementById("slide").appendChild(lists[0]);
@@ -7,6 +8,88 @@ document.getElementById("prev").onclick = function () {
   document.getElementById("slide").prepend(lists[lists.length - 1]);
 };
 
+//map data timeline
+const listChallenger = [
+  {
+    day: "09/10/2023",
+    number: "Thử thách 0",
+    name: "Xây dựng website theo mẫu cho sẵn",
+    link: "https://tt1-ine.vercel.app/",
+  },
+  {
+    day: "17/10/2023",
+    number: "Thử thách 1",
+    name: "Xây dựng website Portfolio cá nhân",
+    link: "https://hoanghh.thuctap.inevn.com/tt1/index.html",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 2",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 3",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 4",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 5",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 6",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 7",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 8",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+  {
+    day: "09/10/2023",
+    number: "Thử thách 9",
+    name: "Coming soon",
+    link: "Coming soon",
+  },
+];
+
+var Challdata = document.querySelector(".box");
+
+document.getElementById("challen").innerHTML = listChallenger
+  .map(
+    (challenger) =>
+      `<li id ="challList">
+        <span class="timeline-point"></span>
+        <span class="date">${challenger.day}</span>
+        <div class="timeline-content">
+          <h1>${challenger.number}</h1>
+          <h2>${challenger.name}</h2>
+          <a href="${challenger.link}">${challenger.link}</a>
+        </div>
+      </li>`
+  )
+  .join("");
+
+//animation progress
 const line = document.querySelector(".timeline-innerline");
 
 let i = 0;
@@ -16,13 +99,16 @@ let target2 = document.querySelectorAll(".timeline ul li");
 
 const timeline_events = document.querySelectorAll("ul li");
 
+console.log(timeline_events);
+
 function showTime(e) {
-  e.setAttribute("done", "true");
   e.querySelector(".timeline-point").style.background = "blue";
   e.querySelector(".date").style.opacity = "100%";
   e.querySelector(".timeline-content").style.opacity = "100%";
   e.querySelector(".timeline-content").style.transform = "translateY(0px)";
 }
+
+console.log(target2);
 
 function slowLoop() {
   setTimeout(function () {
@@ -35,6 +121,7 @@ function slowLoop() {
   }, 800);
 }
 
+//% thanh progress, chuyen doc va ngang
 function timelineProgress(value) {
   let progress = `${(value / timeline_events.length) * 100}%`;
   if (window.matchMedia("(min-width: 870px)").matches) {
@@ -46,6 +133,7 @@ function timelineProgress(value) {
   }
 }
 
+//kiem tra viec giao nhau cua ul li va progress, showTime
 let observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -70,15 +158,4 @@ if (window.matchMedia("(min-width: 870px)").matches) {
   target2.forEach((t) => {
     observer.observe(t);
   });
-}
-
-var doit;
-window.addEventListener("resize", () => {
-  clearTimeout(doit);
-  doit = setTimeout(resizeEnd, 1200);
-});
-
-function resizeEnd() {
-  i = 0;
-  slowLoop();
 }
